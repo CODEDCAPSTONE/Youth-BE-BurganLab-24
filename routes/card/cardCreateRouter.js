b7a8a90b3c6047e220419728ea8eee74834cdeff
meshariSalhouli <mesharialhouli12@gmail.com>
2025-01-21 12:47:59 +0300

    fix import

--- routes/card/cardCreateRouter.js
+++ routes/card/cardCreateRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { body } = require("express-validator");
-const Card = require("../model/card");
-const { requireAuth, validateRequest } = require("../middleware");
+const Card = require("../../model/card");
+const { requireAuth, validateRequest } = require("../../middleware");
 
 const router = express.Router();
 

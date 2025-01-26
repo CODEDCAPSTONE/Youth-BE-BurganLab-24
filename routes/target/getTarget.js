e6b7cc26edfe8976b92f42437d29efb451a2986f
Hussain <hussein-_2000@hotmail.com>
2025-01-26 13:53:30 +0300

    get targets

--- routes/target/getTarget.js
+++ routes/target/getTarget.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const Target = require("../../model/target");
-
+const { requireAuth } = require("../../middleware");
+const User = require("../../model/user");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const userId = req.query.userId;
-
-  const targets = await Target.find({ userId: userId });
+router.get("/", requireAuth, async (req, res) => {
+  const user = await User.findById(req.user.id).populate("targets");
+  const targets = user.targets;
 
   const totalMonthlyDeduction = targets.reduce(
     (sum, target) => sum + target.monthlyDeduction,
